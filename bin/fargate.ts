dac16064d0704d6ab8b8c89f79a68986743e27ec
Mike McGrath <mokeseven7@gmail.com>
2022-07-21 02:27:03 -0600

    refactor

--- bin/fargate.ts
+++ bin/fargate.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { FargateStack } from '../lib/fargate-construct';
+import { FargateStack } from '../lib/fargate-stack';
 
 const app = new cdk.App();
 new FargateStack(app, 'FargateStack', {
